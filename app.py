8067a57613f8f44b08d95a0e8ba5600dcffd1bdd
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-09-28 00:39:51 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import streamlit as st
 
-#loading data 
+# load data
 data = pd.read_csv('creditcard.csv')
 
-#separate 'class' columns
-legit = data['Class'==0]
-fraud = data['Class'==1]
+# separate  transactions
+legit = data[data.Class == 0]
+fraud = data[data.Class == 1]
 
-#balancing the numbers of diff dfs
+# undersample legitimate transaction
 legit_sample = legit.sample(n=len(fraud), random_state=2)
-data = pd.concat([legit_sample,fraud], axis=0)
+data = pd.concat([legit_sample, fraud], axis=0)
 
-#splitting data
+# split data 
 X = data.drop(columns="Class", axis=1)
 y = data["Class"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=2)
 
-#training 
+# train model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-#evalıate model score 
+# evaluate model 
 train_acc = accuracy_score(model.predict(X_train), y_train)
 test_acc = accuracy_score(model.predict(X_test), y_test)
 
-#streamlitapp 
-st.title("Creadit Card Fraud Detection")
-input_df = st.text_input("Enter the following features")
-input_df_splitted = input_df.split(',')
+# Streamlit app
+st.title("Credit Card Fraud Detection Model")
+st.write("Enter the following features to check if the transaction is legitimate or fraudulent:")
 
-submit = st.button("submit")
+input_df = st.text_input('Input All features')
+input_df_lst = input_df.split(',')
+submit = st.button("Submit")
 
-if submit : 
-    features = np_df = np.asarray(input_df_splitted, dtype=np.float64)
+if submit:
     
+    features = np.array(input_df_lst, dtype=np.float64)
+    prediction = model.predict(features.reshape(1,-1))
+    
+
+    if prediction[0] == 0:
+        st.write("Legitimate transaction")
+    else:
+        st.write("Fraudulent transaction")
